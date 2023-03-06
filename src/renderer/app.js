e6fbef96c01ff3da5efd41ba703ea9d817dcf10f
Dmytro <ihnatenko.dev@gmail.com>
2023-03-06 12:11:15 +0200

    Impl show/hide modal, create read-item

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,1 +1,23 @@
-console.log('Hello index');
+window.addEventListener('DOMContentLoaded', () => {
+	const showModalBtn = document.querySelector('#show-modal');
+	const closeModalBtn = document.querySelector('#close-modal');
+	const modal = document.querySelector('#modal');
+	const addUrlInput = document.querySelector('#url');
+	const addUrlBtn = document.querySelector('#add-item');
+
+	showModalBtn.addEventListener('click', () => {
+		modal.style.display = 'flex';
+		addUrlInput.focus();
+	});
+	closeModalBtn.addEventListener('click', () => {
+		modal.style.display = 'none';
+	});
+
+	addUrlBtn.addEventListener('click', () => {
+		if (addUrlInput.value) console.log(addUrlInput.value);
+	});
+
+	addUrlInput.addEventListener('keyup', (e) => {
+		if (e.key === 'Enter') addUrlBtn.click();
+	});
+});
