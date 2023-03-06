0bd945a5bd161455a7253006a6ce1796839fda3f
Dmytro <ihnatenko.dev@gmail.com>
2023-03-06 12:09:42 +0200

    Add mainWindow pos/size state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const { app, BrowserWindow, Menu } = require('electron');
+const mainWindowState = require('electron-window-state');
 const path = require('path');
 
 const createWindow = () => {
+	let stateWin = mainWindowState({
+		defaultWidth: 500,
+		defaultHeight: 650,
+	});
+
 	const mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
+		x: stateWin.x,
+		y: stateWin.y,
+		width: stateWin.width,
+		height: stateWin.height,
+		minWidth: 350,
+		maxWidth: 650,
+		minHeight: 300,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 		},
 	});
 
+	stateWin.manage(mainWindow);
 	mainWindow.loadFile(path.join(__dirname, '/renderer/main.html'));
 	Menu.setApplicationMenu(null);
 	mainWindow.webContents.openDevTools();
